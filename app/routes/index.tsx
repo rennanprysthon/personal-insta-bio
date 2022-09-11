77433e57d58b9560e240ebbcf550063d6dce9ac8
Rennan Prysthon <rennan.prysthon@liferay.com>
2022-09-11 10:51:03 -0300

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,53 @@
+export default function Index() {
+  return (
+    <main>
+      <div className="container">
+        <header className="header">
+          <h1>
+            Rennan Prysthon Links
+          </h1>
+          <a href="http://www.github.com/rennanprysthon" target="_blank">
+            <img src="http://www.github.com/rennanprysthon.png" />
+          </a>
+        </header>
+        <ul className="links-container">
+          <li className="link-item">
+            <a href="http://www.linkedin.com/in/rennanprysthon" target="_blank">
+              Linkedin
+            </a>
+          </li>
+          <li className="link-item">
+            <a href="http://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+              Twitter
+            </a>
+          </li>
+          <li className="link-item">
+            <a href="http://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+              Facebook
+            </a>
+          </li>
+          <li className="link-item">
+            <a href="https://www.github.com/rennanprysthon" target="_blank">
+              Github
+            </a>
+          </li>
+          <li className="link-item">
+            <a href="https://www.instagram.com/rennanprysthon/" target="_blank">
+              Instagram
+            </a>
+          </li>
+          <li className="link-item">
+            <a href="https://rennan-portfolio.netlify.app/" target="_blank">
+              Personal site <br />(ainda não terminei)
+            </a>
+          </li>
+        </ul>
+
+        <footer>
+          Criado por mim mesmo.
+        </footer>
+      </div>
+    </main>
+  );
+
+}
